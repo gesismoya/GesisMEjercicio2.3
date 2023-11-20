c42e6ac376d38dc48f48520e0f1ec77625d45143
gesis.moya <geovannamoyac@gmail.com>
2023-11-19 23:31:16 -0600

    Gesis Moya

---
+++ Ejercicio2.3/app/src/main/java/com/example/ejercicio23/Configuracion/SQLiteConexion.java
@@ -0,0 +1,44 @@
+package com.example.ejercicio23.Configuracion;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class SQLiteConexion extends SQLiteOpenHelper {
+    public SQLiteConexion(Context context, String dbname, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, dbname, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Tabla.CreateTableImagenes);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Tabla.DropeTableImagenes);
+        onCreate(db);
+    }
+
+    public Cursor getAll() {
+        return(getReadableDatabase().rawQuery("SELECT * FROM imagenes",null));
+    }
+    public void insert(byte[] bytes, String descripcion)
+    {
+        ContentValues cv = new ContentValues();
+
+        cv.put(Tabla.blobImagen,bytes);
+        cv.put(Tabla.descripcion,descripcion);
+        Log.e("inserted", "inserted");
+        getWritableDatabase().insert(Tabla.tablaImagenes, Tabla.idImagen,cv);
+
+    }
+    public byte[] getImage(Cursor c)
+    {
+        return(c.getBlob(1));
+    }
+
+}
